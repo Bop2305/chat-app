0814775da77c5502e6538b59d5a5693488757cba
Thong.NC <canhthong05@gmail.com>
2024-10-24 15:42:14 +0700

    add layout

---
+++ client/modules/layouts/Layout.tsx
@@ -0,0 +1,24 @@
+"use client"
+import { connect } from "react-redux"
+import Footer from "./Footer"
+import Header from "./Header"
+
+type LayoutProps = {
+  authenticate: boolean
+  children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ authenticate, children }) => {
+  return <>
+    {authenticate && <Header />}
+    {children}
+    <Footer />
+  </>
+}
+
+const mapStateToProps = (state: any) => ({
+  authenticate: state.auth.authenticate
+})
+
+export default connect(mapStateToProps)(Layout)
+
