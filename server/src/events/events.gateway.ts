cb3b27e7a2108bc2e819d1b8a108019626c27ea8
Thong.NC <canhthong05@gmail.com>
2024-10-24 12:12:53 +0700

    add socket server

---
+++ server/src/events/events.gateway.ts
@@ -0,0 +1,27 @@
+import { SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from "socket.io"
+
+@WebSocketGateway({
+  cors: {
+    origin: 'http://localhost:3000', // Allow requests from your Next.js frontend
+    credentials: true,
+  },
+})
+export class EventsGateway {
+  @WebSocketServer() server: Server
+
+  handleConnection(client: Socket) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: any, payload: any): void {
+    // console.log("[Message Event] [Client] ", client);
+    console.log("[Message Event] [payload] ", payload);
+    this.server.emit("message", payload)
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Allow requests from your Next.js app
+    credentials: true, // Enable if you need to send cookies
+  });
+
+  // Use WebSockets with CORS support
+  app.useWebSocketAdapter(new IoAdapter(app));
+
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
